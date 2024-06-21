f23e0bcaaa26c30b1263f7023cb97434ac86e73e
sickmitch <marinic.michele@proton.me>
2024-06-21 17:03:05 +0200

    link fix

--- systemd/user/nfs_share.sh
+++ systemd/user/nfs_share.sh
@@ -1,1 +1,38 @@
-/home/mike/Documenti/Tech/script/local/system/nfs_share.sh
+#!/bin/bash
+
+# SSID target
+TARGET_SSID="MeS"
+
+# NFS server and share
+NFS_SERVER="192.168.1.20"
+NFS_SHARE="/"
+MOUNT_POINT="/home/mike/NAS/"
+
+# Check if connected to the target SSID
+current_ssid=$(iwgetid -r)
+
+if [ "$current_ssid" == "$TARGET_SSID" ]; then
+    echo "Connected to $TARGET_SSID."
+
+    # Check if mount point exists, if not, create it
+    if [ ! -d "$MOUNT_POINT" ]; then
+        mkdir -p "$MOUNT_POINT"
+    fi
+
+    # Check if already mounted
+    if mount | grep -q "$MOUNT_POINT"; then
+        echo "NFS share already mounted."
+    else
+        # Mount the NFS share
+        sudo mount -t nfs "$NFS_SERVER:$NFS_SHARE" "$MOUNT_POINT"
+        
+        if [ $? -eq 0 ]; then
+            echo "NFS share mounted successfully."
+        else
+            echo "Failed to mount NFS share."
+        fi
+    fi
+else
+    echo "Not connected to $TARGET_SSID. No action taken."
+fi
+
