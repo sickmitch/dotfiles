50736157819aec7d610eee0330f0ac8d34aafaf7
sickmitch <marinic.michele@proton.me>
2025-06-30 08:45:36 +0200

    twirks, tailscale integration

--- systemd/user/check-battery.sh
+++ systemd/user/check-battery.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-CHECK=`acpi -b | grep "Battery 0" |  grep "Discharging" | wc -l`   #if the battery is discharging return 1
-RES=`acpi -b | grep "Battery 0" | cut -d " " -f 5` #take out the time field of acpi -b
+CHECK=$(acpi -b | grep "Battery 0" | grep "Discharging" | wc -l) #if the battery is discharging return 1
+RES=$(acpi -b | grep "Battery 0" | cut -d " " -f 5)              #take out the time field of acpi -b
 
-if [[ $CHECK == 1 ]] #check if pc is in charge
-then
-  if [[ $RES < 00:30:00 ]] ; then #time left on battery trigger
-      DISPLAY=:0 /usr/bin/notify-send -u critical "$(acpi -b | grep "Battery 0")" #notify
+if [[ $CHECK == 1 ]]; then                                                      #check if pc is in charge
+  if [[ $RES < 00:30:00 ]]; then                                                #time left on battery trigger
+    DISPLAY=:0 /usr/bin/notify-send -u critical "$(acpi -b | grep "Battery 0")" #notify
   fi
 else
-  exit 2
+  exit 0
 fi
 
 exit
--- systemd/user/nfs_share.sh
+++ systemd/user/nfs_share.sh
@@ -8,39 +8,36 @@ NFS_SERVER="192.168.1.20"
 NFS_SHARES=(
     "/share/:/home/mike/NAS/"
 )
-
-# Check if connected to one of the target SSIDs or to NordVPN server dl360
 current_ssid=$(iwgetid -r)
-nordvpn_status=$(nordvpn status | sed -n 's/.*Server: \(.*\)/\1/p')
 
-if [[ " ${TARGET_SSIDS[@]} " =~ " ${current_ssid} " ]] || [[ "$nordvpn_status" == "dl360" ]]; then
-    echo "Connected to Home or NordVPN server dl360."
+# Loop through each share mapping
+for share_mapping in "${NFS_SHARES[@]}"; do
+    IFS=':' read -r share mount_point <<< "$share_mapping"
 
-    # Loop through each share
-    for share_mapping in "${NFS_SHARES[@]}"; do
-        IFS=':' read -r share mount_point <<< "$share_mapping"
-        
-        # Check if mount point exists, if not, create it
-        if [ ! -d "$mount_point" ]; then
-            mkdir -p "$mount_point"
-        fi
-
-        # Check if already mounted
-        if mount | grep -q "$mount_point"; then
-            echo "NFS share $share already mounted at $mount_point"
-        else
-            # Mount the NFS share
-            sudo mount -t nfs "$NFS_SERVER:$share" "$mount_point"
-            if [ $? -eq 0 ]; then
-                echo "NFS share $share mounted successfully at $mount_point"
-            else
-                echo "Failed to mount NFS share $share at $mount_point"
-                DISPLAY=:0 /usr/bin/notify-send -u critical "Failed to mount NAS" #notify
-            fi
-        fi
-    done
-else
-    echo "Not connected to any target SSID or NordVPN server dl360. No action taken."
-    DISPLAY=:0 /usr/bin/notify-send -u critical "NAS not available" #notify
+# Check if mount point exists, if not, create it
+if [ ! -d "$mount_point" ]; then
+    mkdir -p "$mount_point"
 fi
 
+mount_nfs() {
+    if sudo mount -t nfs "$NFS_SERVER:$share" "$mount_point"; then
+        echo "NFS share $share mounted successfully at $mount_point"
+    else
+        echo "Failed to mount NFS share $share at $mount_point"
+        DISPLAY=:0 /usr/bin/notify-send -u critical "Failed to mount NAS" #notify
+        exit 2
+    fi
+}
+
+if [[ " ${TARGET_SSIDS[*]} " =~ ${current_ssid} ]]; then
+        mount_nfs
+    else
+        if sudo -u mike tailscale up --accept-dns --accept-routes; then
+            echo "Tailscale up successful"
+            mount_nfs
+        else
+            echo "Tailscale up failed"
+            DISPLAY=:0 /usr/bin/notify-send -u critical "Failed to connect to Tailscale" #notify
+        fi
+    fi
+done
