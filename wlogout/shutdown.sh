29425af6e745d184049e237a3d9a2f7e63235e12
sickmitch <marinic.michele@proton.me>
2025-07-19 06:01:15 +0200

    update

--- wlogout/shutdown.sh
+++ wlogout/shutdown.sh
@@ -3,12 +3,13 @@
 # Tutto ciò che và fatto prima di spegnere il sistema
 # - Smonto il NAS
 # - Disattivo Tailscale
+# - Chiudo Chrome
 
 # Mando una notifica per conferma che lo script è partito
 if [ "$1" = "reboot" ]; then
-  zenity --warning --text="Rebooting the system" --title="System Notice"
+  DISPLAY=:0 /usr/bin/notify-send -u critical "Rebooting the system" #notify
 elif [ "$1" = "shutdown" ]; then
-  zenity --warning --text="Shutting down the system" --title="System Notice"
+  DISPLAY=:0 /usr/bin/notify-send -u critical "Shutting down the system" #notify
 fi
 
 # Smonto il NAS
@@ -21,6 +22,19 @@ fi
 if [ "$(ip a | grep -c "100.64.224.43")" -gt 0 ]; then
   tailscale down
   sleep 3
+fi
+
+# Chiudo Chrome 
+if pgrep chrome > /dev/null; then
+  echo "Closing Google Chrome gracefully…"
+  pkill -TERM chrome
+
+  # Wait for Chrome to exit (max 10 seconds)
+  timeout=10
+  while pgrep chrome >/dev/null && [ $timeout -gt 0 ]; do
+    sleep 1
+    ((timeout--))
+  done
 fi
 
 if [ "$1" = "reboot" ]; then
