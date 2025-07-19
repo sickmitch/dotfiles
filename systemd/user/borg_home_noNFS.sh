4cb8189287f334b8cb2338282072c7690104a756
sickmitch <marinic.michele@proton.me>
2025-07-19 05:59:54 +0200

    systemd services tweaks

--- systemd/user/borg_home_noNFS.sh
+++ systemd/user/borg_home_noNFS.sh
@@ -15,16 +15,16 @@ MOUNTED_PATHS=$(findmnt -n -l -o TARGET | grep "^$BACKUP_PATH" || true)
 # Build exclude patterns for mounted paths
 EXCLUDE_OPTS=""
 while IFS= read -r mount_point; do
-    if [ ! -z "$mount_point" ]; then
-        EXCLUDE_OPTS="$EXCLUDE_OPTS --exclude '$mount_point'"
-    fi
-done <<< "$MOUNTED_PATHS"
+  if [ -n "$mount_point" ]; then
+    EXCLUDE_OPTS="$EXCLUDE_OPTS --exclude '$mount_point'"
+  fi
+done <<<"$MOUNTED_PATHS"
 
 # Add any additional excludes you might want
 EXCLUDE_OPTS="$EXCLUDE_OPTS --exclude '/home/mike/*.tmp' --exclude '/home/mike/*.temp' --exclude '/home/mike/.cache/*'"
 
 # Create backup command
-BACKUP_CMD="sudo borg create \
+BACKUP_CMD="borg create \
     --stats \
     --progress \
     --compression lz4 \
@@ -33,32 +33,29 @@ BACKUP_CMD="sudo borg create \
     $BACKUP_PATH"
 
 # Prune command
-PRUNE_CMD="sudo borg prune \
+PRUNE_CMD="borg prune \
     -v --list
     $REPOSITORY \
     --keep-within 1d --keep-daily 7"
 
-
 # Execute the backup
 eval $BACKUP_CMD
 if [ $? -eq 0 ]; then
-    BACK=1
+  BACK=1
 fi
 
 # Prune repo
 eval $PRUNE_CMD
 if [ $? -eq 0 ]; then
-    PRUNE=1
+  PRUNE=1
 fi
 
-if [ $PRUNE -eq 0 ] && [ $BACK -eq 0 ]; then
-    systemd-notify --status="Failed"
-elif [ $PRUNE -eq 1 ] && [ $BACK -eq 0 ]; then
-    systemd-notify --status="Backup Failed"
-elif [ $PRUNE -eq 0 ] && [ $BACK -eq 1 ]; then
-    systemd-notify --status="Prune Failed"
-elif [ $PRUNE -eq 1 ] && [ $BACK -eq 1 ]; then
-    systemd-notify --status="$(date +%H:%M) - Success"
-fi
-
-
+# if [ $PRUNE -eq 0 ] && [ $BACK -eq 0 ]; then
+#   systemd-notify --status="Failed"
+# elif [ $PRUNE -eq 1 ] && [ $BACK -eq 0 ]; then
+#   systemd-notify --status="Backup Failed"
+# elif [ $PRUNE -eq 0 ] && [ $BACK -eq 1 ]; then
+#   systemd-notify --status="Prune Failed"
+# elif [ $PRUNE -eq 1 ] && [ $BACK -eq 1 ]; then
+#   systemd-notify --status="$(date +%H:%M) - Success"
+# fi
--- systemd/user/check-battery.sh
+++ systemd/user/check-battery.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CHECK=$(acpi -b | grep "Battery 0" | grep "Discharging" | wc -l) #if the battery is discharging return 1
-RES=$(acpi -b | grep "Battery 0" | cut -d " " -f 5)              #take out the time field of acpi -b
+CHECK=$(acpi -b | grep "Battery 0" | grep -c "Discharging")        #if the battery is discharging return 1
+RES=$(acpi -b | grep "Battery 0" | cut -d " " -f 4 | sed 's/%,//') #take out battery level
 
-if [[ $CHECK == 1 ]]; then                                                      #check if pc is in charge
-  if [[ $RES < 00:30:00 ]]; then                                                #time left on battery trigger
+if [[ $CHECK -eq 1 ]]; then                                                     #check if pc discharging
+  if [[ $RES -lt 11 ]]; then                                                    #check if percentage is under 11
     DISPLAY=:0 /usr/bin/notify-send -u critical "$(acpi -b | grep "Battery 0")" #notify
   fi
 else
