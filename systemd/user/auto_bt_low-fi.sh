5ce56c6d8ee6238ae9f4c2fba4fdd967506f9104
sickmitch <marinic.michele@proton.me>
2025-08-28 06:33:00 +0200

    firefox back and twirks

--- systemd/user/auto_bt_low-fi.sh
+++ systemd/user/auto_bt_low-fi.sh
@@ -3,7 +3,7 @@ device_mac="00:1B:66:0F:3D:F0"
 
 while true; do
   if bluetoothctl info "$device_mac" | grep -q "Connected: yes"; then
-    cvlc --no-video "$(yt-dlp -f bestaudio --get-url https://youtu.be/jfKfPfyJRdk)"
+    cvlc http://stream.laut.fm/lofi
   fi
   sleep 5
 done
