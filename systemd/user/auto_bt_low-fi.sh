c855766334b35a26088d93ec8af76c2e80b51588
sickmitch <marinic.michele@proton.me>
2025-09-01 07:18:28 +0200

    radio fix

--- systemd/user/auto_bt_low-fi.sh
+++ systemd/user/auto_bt_low-fi.sh
@@ -3,7 +3,7 @@ device_mac="00:1B:66:0F:3D:F0"
 
 while true; do
   if bluetoothctl info "$device_mac" | grep -q "Connected: yes"; then
-    cvlc http://stream.laut.fm/lofi
+    cvlc https://streams.fluxfm.de/Chillhop/mp3-320/streams.fluxfm.de/
   fi
   sleep 5
 done
